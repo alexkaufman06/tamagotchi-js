65b403fee1fc7ce69ea77e2594c54c6ec418ec11
Alex Kaufman <alexkaufman06@gmail.com>
2016-06-23 08:48:55 -0700

    Add player and currency counter

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,34 @@
 $(document).ready(function() {
 
-  var tamagotchiOne = { food: 100 }
+  var player = { pennies: 0, dimes: 0, dollars: 0 };
 
-  var counter = setInterval(timer, 500);
+  var tamagotchiOne = { food: 100 };
+
+  var counter = setInterval(timer, 250);
 
   function timer()
   {
-    tamagotchiOne.food = tamagotchiOne.food - 1;
+    player.pennies += 1;
+
+    tamagotchiOne.food = tamagotchiOne.food - .5;
+
+    if (player.pennies === 10) {
+      player.dimes += 1;
+      player.pennies -=10;
+      $('.dimes').empty();
+      $('.dimes').append(player.dimes);
+    }
+
+    if (player.dimes === 10) {
+      player.dollars += 1;
+      player.dimes -=10;
+      $('.dollars').empty();
+      $('.dollars').append(player.dollars);
+    }
 
     $('#food').css('width', tamagotchiOne.food + "%");
+    $('.pennies').empty();
+    $('.pennies').append(player.pennies);
   }
 
 });
