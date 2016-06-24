75ad8475ff80f8cba6f6f32d1800e56733ab8413
Alex Kaufman <alexkaufman06@gmail.com>
2016-06-24 08:20:54 -0700

    Fix currency counter and update css

--- js/scripts.js
+++ js/scripts.js
@@ -6,22 +6,22 @@ $(document).ready(function() {
 
   var counter = setInterval(timer, 250);
 
-  function timer()
-  {
+  function timer() {
     player.pennies += 1;
-
     tamagotchiOne.food = tamagotchiOne.food - .5;
 
     if (player.pennies === 10) {
-      player.dimes += 1;
+      player.dimes += 10;
       player.pennies -=10;
       $('.dimes').empty();
       $('.dimes').append(player.dimes);
     }
 
-    if (player.dimes === 10) {
+    if (player.dimes === 100) {
       player.dollars += 1;
-      player.dimes -=10;
+      player.dimes -= 100;
+      $('.dimes').empty();
+      $('.dimes').append(player.dimes);
       $('.dollars').empty();
       $('.dollars').append(player.dollars);
     }
