1afd4c550fab550bfca5c40efb6f2bd073df5444
Alex Kaufman <alexkaufman06@gmail.com>
2016-06-24 08:31:33 -0700

    Minify css, add activity level and sleep level

--- js/scripts.js
+++ js/scripts.js
@@ -2,13 +2,15 @@ $(document).ready(function() {
 
   var player = { pennies: 0, dimes: 0, dollars: 0 };
 
-  var tamagotchiOne = { food: 100 };
+  var tamagotchiOne = { food: 100, activity: 100, sleep: 100 };
 
   var counter = setInterval(timer, 250);
 
   function timer() {
     player.pennies += 1;
-    tamagotchiOne.food = tamagotchiOne.food - .5;
+    tamagotchiOne.food -= .5;
+    tamagotchiOne.activity -= .5;
+    tamagotchiOne.sleep -= .5;
 
     if (player.pennies === 10) {
       player.dimes += 10;
@@ -27,6 +29,8 @@ $(document).ready(function() {
     }
 
     $('#food').css('width', tamagotchiOne.food + "%");
+    $('#activity').css('width', tamagotchiOne.activity + "%");
+    $('#sleep').css('width', tamagotchiOne.sleep + "%");
     $('.pennies').empty();
     $('.pennies').append(player.pennies);
   }
