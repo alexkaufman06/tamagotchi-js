4a4fbb345765b4a8f27636ba5d7d9633280f0c9a
Alex Kaufman <alexkaufman06@gmail.com>
2016-07-04 09:57:55 -0700

    Rebuild shop logic

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,7 @@ $(document).ready(function() {
 
   var player = { pennies: 0, dimes: 0, dollars: 0 };
 
-  $('.money').empty();
-  $('.money').append(player.money);
-
-  var tamagotchiOne = { food: 100, activity: 100, sleep: 100, isDead: function() {
+  var tamagotchi = { food: 100, activity: 100, sleep: 100, money: 0, isDead: function() {
       if ((this.food <= 0) || (this.activity <= 0) || (this.sleep <= 0)) {
         return true;
       } else {
@@ -20,147 +17,131 @@ $(document).ready(function() {
 
   var time = 0;
   var difficulty = 0;
-
   var counter = setInterval(timer, 250);
 
   // functions for buttons (refactor)
 
   $('#feed').click(function() {
-    tamagotchiOne.food += 5;
+    tamagotchi.food += 5;
   });
 
   $('#play').click(function() {
-    tamagotchiOne.activity += 5;
+    tamagotchi.activity += 5;
   });
 
   $('#sleep').click(function() {
-    tamagotchiOne.sleep += 5;
+    tamagotchi.sleep += 5;
+  });
+
+  // shop buttons
+
+  $('#buy-potion').on('click', 'img', function () {
+    tamagotchi.food += 10;
+    tamagotchi.activity += 10;
+    tamagotchi.sleep += 10;
+    tamagotchi.money -= .5;
+    $('.money').empty();
+    $('.money').append(Math.round(tamagotchi.money * 100)/100);
   });
 
   function timer() {
-    player.pennies += 1;
-    tamagotchiOne.food -= (.5 + difficulty);
-    tamagotchiOne.activity -= (.5 + difficulty);
-    tamagotchiOne.sleep -= (.5 + difficulty);
+    // item controls
+
+    if (tamagotchi.money < .5) {
+      $('#potion').show();
+      $('#buy-potion').hide();
+    }
+
+    if (tamagotchi.money > .49) {
+      $('#potion').hide();
+      $('#buy-potion').removeClass('hide');
+      $('#buy-potion').show();
+    }
+
+    tamagotchi.money += .01;
+    tamagotchi.food -= (.5 + difficulty);
+    tamagotchi.activity -= (.5 + difficulty);
+    tamagotchi.sleep -= (.5 + difficulty);
     time += .25;
 
     if (time % 5 === 0) {
       difficulty += .5;
     }
 
-    if (tamagotchiOne.isDead() === true) {
+    if (tamagotchi.isDead() === true) {
       clearInterval(counter);
-      alert("This webpage regrets to inform you that your tamagotchi has passed away after a long life of " + time + "s. Don't worry, you can try again!");
+      alert("This webpage regrets to inform you that your tamagotchi has passed away after a long life of " + time + "seconds. Don't worry, you can try again!");
       location.reload();
-    }
-
-    if (player.pennies === 10) {
-      player.dimes += 1;
-      player.pennies -=10;
-      $('.dimes').empty();
-      $('.dimes').append(player.dimes);
-    }
-
-    if (player.dimes === 10) {
-      player.dollars += 1;
-      player.dimes -= 10;
-      $('.dimes').empty();
-      $('.dimes').append(player.dimes);
-      $('.dollars').empty();
-      $('.dollars').append(player.dollars);
     }
 
     // logic to stop level from going above 100 (refactor)
 
-    if (tamagotchiOne.food > 100) {
-      tamagotchiOne.food = 100;
+    if (tamagotchi.food > 100) {
+      tamagotchi.food = 100;
     }
 
-    if (tamagotchiOne.activity > 100) {
-      tamagotchiOne.activity = 100;
+    if (tamagotchi.activity > 100) {
+      tamagotchi.activity = 100;
     }
 
-    if (tamagotchiOne.sleep > 100) {
-      tamagotchiOne.sleep = 100;
+    if (tamagotchi.sleep > 100) {
+      tamagotchi.sleep = 100;
     }
 
     // logic for changing color of progress bars (refactor)
 
-    if (tamagotchiOne.food > 65) {
+    if (tamagotchi.food > 65) {
       $('#food-level').removeClass();
       $('#food-level').addClass('progress-bar progress-bar-success');
-    } else if (tamagotchiOne.food < 65 && tamagotchiOne.food > 30) {
+    } else if (tamagotchi.food < 65 && tamagotchi.food > 30) {
       $('#food-level').removeClass();
       $('#food-level').addClass('progress-bar progress-bar-warning');
-    } else if (tamagotchiOne.food < 30) {
+    } else if (tamagotchi.food < 30) {
       $('#food-level').removeClass();
       $('#food-level').addClass('progress-bar progress-bar-danger');
     }
 
-    if (tamagotchiOne.activity > 65) {
+    if (tamagotchi.activity > 65) {
       $('#activity-level').removeClass();
       $('#activity-level').addClass('progress-bar progress-bar-success');
-    } else if (tamagotchiOne.activity < 65 && tamagotchiOne.activity > 30) {
+    } else if (tamagotchi.activity < 65 && tamagotchi.activity > 30) {
       $('#activity-level').removeClass();
       $('#activity-level').addClass('progress-bar progress-bar-warning');
-    } else if (tamagotchiOne.activity < 30) {
+    } else if (tamagotchi.activity < 30) {
       $('#activity-level').removeClass();
       $('#activity-level').addClass('progress-bar progress-bar-danger');
     }
 
-    if (tamagotchiOne.sleep > 65) {
+    if (tamagotchi.sleep > 65) {
       $('#sleep-level').removeClass();
       $('#sleep-level').addClass('progress-bar progress-bar-success');
-    } else if (tamagotchiOne.sleep < 65 && tamagotchiOne.sleep > 30) {
+    } else if (tamagotchi.sleep < 65 && tamagotchi.sleep > 30) {
       $('#sleep-level').removeClass();
       $('#sleep-level').addClass('progress-bar progress-bar-warning');
-    } else if (tamagotchiOne.sleep < 30) {
+    } else if (tamagotchi.sleep < 30) {
       $('#sleep-level').removeClass();
       $('#sleep-level').addClass('progress-bar progress-bar-danger');
-    }
-
-    // item controls
-    if (player.dimes < 5) {
-      $('#potion').addClass('gray-item');
-    }
-
-    if (player.dimes > 4) {
-      $('#potion').removeClass('gray-item');
-
-      $('body').on('click','img', function() {
-        tamagotchiOne.food += 10;
-        tamagotchiOne.activity += 10;
-        tamagotchiOne.sleep += 10;
-        player.dimes -= 5;
-        $('.dimes').empty();
-        $('.dimes').append(player.dimes);
-        $('#potion').addClass('gray-item');
-
-        if (player.dimes > 4) {
-          $('#potion').removeClass('gray-item');
-        }
-      });
     }
 
     // computer keyboard controls
 
     $(document).keydown(function(e) {
       if (e.which === 37) {
-        tamagotchiOne.food += .025;
+        tamagotchi.food += .025;
       } else if (e.which === 40) {
-        tamagotchiOne.activity += .025;
+        tamagotchi.activity += .025;
       } else if (e.which === 39) {
-        tamagotchiOne.sleep += .025;
+        tamagotchi.sleep += .025;
       }
     });
 
-    // logic for changing value of progress bars and updating penny count
+    // logic for changing value of progress bars and updating money count
 
-    $('#food-level').css('width', tamagotchiOne.food + "%");
-    $('#activity-level').css('width', tamagotchiOne.activity + "%");
-    $('#sleep-level').css('width', tamagotchiOne.sleep + "%");
-    $('.pennies').empty();
-    $('.pennies').append(player.pennies);
+    $('#food-level').css('width', tamagotchi.food + "%");
+    $('#activity-level').css('width', tamagotchi.activity + "%");
+    $('#sleep-level').css('width', tamagotchi.sleep + "%");
+    $('.money').empty();
+    $('.money').append(Math.round(tamagotchi.money * 100)/100);
   }
 
 });
